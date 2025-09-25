b4292be9db9149e93e4014233a03394efa172838
Diego Araneda <daranedag@gmail.com>
2025-09-25 15:59:33 -0300

    update de vite

--- src/pages/plataforma/IndicadoresEntorno.jsx
+++ src/pages/plataforma/IndicadoresEntorno.jsx
@@ -12,8 +12,10 @@ export const IndicadoresEntorno = () => {
                 <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Indicadores de Entorno</h2>
                     <p className="mb-4 text-justify">
-                        Estos indicadores son utilizados para medir el impacto de factores externos...
-                        Pueden incluir variables económicas, sociales, políticas y ambientales que afectan el desempeño de ...
+                        Son indicadores que entregan un marco de contexto para la región en ámbitos económicos, sociales,
+                        territoriales y ambientales. Si bien no están directamente vinculados a las metas de la Política
+                        Regional de Fomento Productivo, Emprendimiento e Innovación, se relacionan con sus
+                        lineamientos y permiten interpretar de manera integral los avances y desafíos de la política.
                     </p>
                 </div>
                 <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
